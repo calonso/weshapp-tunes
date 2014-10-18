3395fba99e749a1e063336b09850ea39db9c6b8a
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2014-10-18 18:11:00 +0100

    added objective C songs out streamer

---
+++ WeshApp-tunes/WeshApp-tunes/ObjSong.h
@@ -0,0 +1,13 @@
+//
+//  ObjSong.h
+//  WeshApp-tunes
+//
+//  Created by Carlos Alonso on 18/10/2014.
+//  Copyright (c) 2014 Foot Clan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjSong : NSObject
+
+@end
